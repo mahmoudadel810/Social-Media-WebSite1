9be349fc31b51c3e601cc22d88d35d623e4fb756
mahmoudadel810 <128296830+mahmoudadel810@users.noreply.github.com>
2023-04-29 23:59:58 +0200

    Update connection.js
    Update

--- DB/connection.js
+++ DB/connection.js
@@ -5,5 +5,5 @@ export const connectionDB = async () =>
     return await mongoose
         .connect(process.env.DB_CLOUD)
         .then((res) => { console.log('DB CONNECTED Done........!'); })
-        .catch((err)=>{console.log('DB Connection FAILEDD ..!!!!');})
+        .catch((err)=>{console.log('DB Connection DROPPED  ..!!!!');})
 } 
