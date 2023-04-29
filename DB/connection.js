74bd7f33e13a2b5a28827bbae6031ba0713ba9e8
mahmoudadel810 <128296830+mahmoudadel810@users.noreply.github.com>
2023-04-30 01:18:52 +0200

    CLOUD SOCIALMEDIA

--- DB/connection.js
+++ DB/connection.js
@@ -3,7 +3,7 @@ import mongoose from 'mongoose';
 export const connectionDB = async () =>
 {
     return await mongoose
-        .connect(process.env.DB_CLOUD)
+        .connect(process.env.DB_CLOUD) 
         .then((res) => { console.log('DB CONNECTED Done........!'); })
         .catch((err)=>{console.log('DB Connection DROPPED  ..!!!!');})
 } 
